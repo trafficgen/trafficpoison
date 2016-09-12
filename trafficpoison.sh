766274bc880f0990d61183fb8bf05c68743b7337
Fanis Siampos <fsiamp@gmail.com>
2016-09-12 17:56:21 +0300

    Removed comments that are already mentioned in documentation

--- trafficpoison.sh
+++ trafficpoison.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Ανάλογα το είδος ethernet μπορείτε να αλλάξτε το eth1 σε eth0 
-# Χρησιμοποιούνται τα εργαλεία nemesis,sendip,mausezahn,yersinia,xdotool,hping,nping,idswakeup,stick,snot,rule2alert,sneeze, packeth,epb, scapy , metasploit, bittwist και pfsend
-# Δεν είναι αναγκαία η εγκατάσταση όλων των παραπάνω
-# Για κάθε εκτέλεση θα πρέπει να έχετε ενεργοποιημένα root privileges
-# Για τερματισμό λειτουργίας του προγράμματος πατήστε Ctrl+c συνεχόμενα
-# Ενδεχομένως να χρειαστεί να πατάτε Enter στην περίπτωση όπου εμφανίζεται εκτέλεση DOS επίθεσης από το yersinia για να σταματάτε κάθε φορά την επίθεση
 
 # Πιθανά πρωτόκολλα χρήσης από το nemesis
 
